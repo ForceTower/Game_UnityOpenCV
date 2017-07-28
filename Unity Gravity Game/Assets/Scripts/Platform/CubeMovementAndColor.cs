f3f66e53d08ae9b08141b62f4c0e5e7d6f597662
João Paulo <joaopaulo761@gmail.com>
2017-07-28 14:53:16 -0300

    Player and Game Controller Scripts

--- Unity Gravity Game/Assets/Scripts/Platform/CubeMovementAndColor.cs
+++ Unity Gravity Game/Assets/Scripts/Platform/CubeMovementAndColor.cs
@@ -3,51 +3,64 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CubeMovementAndColor : MonoBehaviour {
-    private float deltaTime;
-    private bool ready;
+    private float _DeltaTime;
+    private bool _Ready;
 
-    private Vector3 startPosition;
-    private Vector3 endPosition;
-    private Color startColor;
-    private Color endColor;
-    private float expectedTime;
+    private Vector3 _StartPosition;
+    private Vector3 _EndPosition;
+    private Color _StartColor;
+    private Color _EndColor;
+    private float _ExpectedTime;
+
+    private PlayerController.GravityModifier _CurrentGravity;
 
     // Use this for initialization
     void Start () {
-        deltaTime = 0;
-        ready = false;
-	}
+        _DeltaTime = 0;
+        _Ready = false;
+    }
 	
-	// Update is called once per frame
-	void Update () {
-        if (ready)
+    // Update is called once per frame
+    void Update () {
+        if (_Ready)
             ChangeColorAndPosition ();
-	}
+    }
 
     public void ChangeToColorAndPosition(Vector3 position, Color color, float time) {
-        ready = true;
-        deltaTime = 0;
-        startPosition = transform.position;
-        startColor = GetComponent<MeshRenderer> ().material.color;
+        _Ready = true;
+        _DeltaTime = 0;
+        _StartPosition = transform.position;
+        _StartColor = GetComponent<MeshRenderer> ().material.color;
 
-        endPosition = position;
-        endColor = color;
-        expectedTime = time;
+        _EndPosition = position;
+        _EndColor = color;
+        _ExpectedTime = time;
     }
 
     private void ChangeColorAndPosition () {
-        if (deltaTime >= 1) {
-            ready = false;
+        if (_DeltaTime >= 1) {
+            _Ready = false;
             OnFinish ();
-            deltaTime = 0;
+            _DeltaTime = 0;
         } else {
-            deltaTime += Time.deltaTime / expectedTime;
-            transform.position = Vector3.Lerp (startPosition, endPosition, deltaTime);
-            GetComponent<MeshRenderer> ().material.color = Color.Lerp (startColor, endColor, deltaTime);
+            _DeltaTime += Time.deltaTime / _ExpectedTime;
+            transform.position = Vector3.Lerp (_StartPosition, _EndPosition, _DeltaTime);
+            GetComponent<MeshRenderer> ().material.color = Color.Lerp (_StartColor, _EndColor, _DeltaTime);
         }
     }
 
     void OnFinish () {
+        if (_EndColor == Color.red)
+            _CurrentGravity = PlayerController.GravityModifier.UP;
+        else if (_EndColor == Color.blue)
+            _CurrentGravity = PlayerController.GravityModifier.DOWN;
+        else if (_EndColor == Color.green)
+            _CurrentGravity = PlayerController.GravityModifier.LEFT;
+        else if (_EndColor == Color.yellow)
+            _CurrentGravity = PlayerController.GravityModifier.RIGHT;
+        else if (_EndColor == Color.black)
+            _CurrentGravity = PlayerController.GravityModifier.NONE;
 
+        gameObject.tag = "Gravity Type " + _CurrentGravity;
     }
 }
---
+++ Unity Gravity Game/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public enum GravityModifier {
+        NONE = 0,
+        UP = 1,
+        DOWN = 2,
+        LEFT = 3,
+        RIGHT = 4
+    };
+
+    private GravityModifier _CurrentGravity;
+
+    public float _GravityForce;
+    public float _MaxVelocity;
+
+    // Use this for initialization
+    void Start () {
+		
+    }
+	
+    // Update is called once per frame
+    void Update () {
+		
+    }
+}
