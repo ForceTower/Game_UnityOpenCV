8f35bd3a2c65c1bdedb6a1d4bc1eca1228c5cf28
Alberto Junior <alberto.junior.1995@gmail.com>
2017-07-27 21:49:16 -0300

    - create script of move plataforms
    - create my scene DevelopmentAlberto

---
+++ Unity Gravity Game/Assets/Scripts/Move.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+    public Vector3 endMarker;
+    public float speed = 1.0F;
+    public float margin;
+    public bool go;
+
+    private Vector3 startMarker;
+    private float startTime;
+    private float journeyLength;
+    private bool startGo;
+    private bool end;
+
+    void Start() {
+        if (margin == 0)
+            margin = 0.1f;
+
+    }
+
+    void Update() {
+         if (go || startGo)
+            move();
+    }
+
+    public void move() {
+        if (go) {
+            startMarker = transform.position;
+            startTime = Time.time;
+            journeyLength = Vector3.Distance(startMarker, endMarker);
+            go = !go;
+            startGo = true;
+            end = false;
+        }
+        if(!end)
+            move(endMarker);
+    }
+
+    public void move(Vector3 endMarker) {
+        float distCovered = (Time.time - startTime) * speed;
+        float fracJourney = distCovered / journeyLength;
+        transform.position = Vector3.Lerp(startMarker, endMarker, fracJourney);
+        // verifiyng arrived at destiny
+        float actualDistance = Vector3.Distance(transform.position, endMarker);
+
+        if (actualDistance < margin)
+            arrivedDestiny();
+    }
+
+    public bool arrivedDestiny() {
+        end = true;
+        startGo = false;
+        return end;
+    }
+}
