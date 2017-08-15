5de1942bd2c0d2b67e98b391e6fbfc6bbe25cfb9
João Paulo <joaopaulo761@gmail.com>
2017-08-15 00:38:01 -0300

    Enemy Finished

--- Unity Gravity Game/Assets/Scripts/Enemy/StationaryEnemy.cs
+++ Unity Gravity Game/Assets/Scripts/Enemy/StationaryEnemy.cs
@@ -3,14 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class StationaryEnemy : MonoBehaviour {
+    public Transform LeftShoot;
+    public Transform RightShoot;
+    public bool Shooting;
+
+    public float ReloadTime;
+    private Transform left;
+    private Transform positions;
+    private Transform right;
 
     // Use this for initialization
     void Start () {
-		
+        
+        Shooting = true;
+
+        positions = transform.GetChild (0);
+        right = positions.GetChild (0);
+        left = positions.GetChild (1);
+
+        StartCoroutine (Shoot ());
     }
-	
-    // Update is called once per frame
-    void Update () {
-		
+
+    public IEnumerator Shoot() {
+        while (Shooting) {
+            Instantiate (LeftShoot, left.position, Quaternion.identity);
+            Instantiate (RightShoot, right.position, Quaternion.identity);
+            yield return new WaitForSeconds (ReloadTime);
+        }
     }
 }
